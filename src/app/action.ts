7be8b9f7cd18fc7cde31dbef7bc83cc809550821
Muhammad Sufiyan Baig <send.sufiyan@gmail.com>
2024-03-28 16:52:44 +0500

    Update action.ts

--- src/app/action.ts
+++ src/app/action.ts
@@ -1,7 +1,23 @@
 'use server';
 
+import { revalidateTag } from "next/cache";
+
 export const getTask = async (url: string) => {
-    const fetchTask = await fetch(url);
+    const fetchTask = await fetch(url,{
+      next :{
+        tags: ['task']
+      }
+    });
     const res = fetchTask.json()
     return res
+  }
+
+export const addTask = async (url: string, data:any) => {
+    const fetchTask = await fetch(url,{
+      method: "POST",
+      headers: {"Content-Type":"application/json"},
+      body: JSON.stringify(data),
+    });
+    revalidateTag('task');
+    return "Task added....."
   }
